3b1167a43a6463340dc189f924f4c4577b04b322
videbian <vinicius.debian@btsglobalbank.com>
2025-06-23 22:59:06 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Configurar CORS para permitir acesso do Frontend
+
+  // Configuração CORS
   app.enableCors({
-    origin: ['https://minha-plataforma-frontend-git-main-vinicius-debians-projects.vercel.app', 'http://localhost:3000'],
+    origin: 'https://minha-plataforma-frontend-m1eyg1r7a-vinicius-debians-projects.vercel.app', // Permite apenas o seu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   } );
-  
-  // Usar a porta fornecida pelo Railway ou 3000 como fallback
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
-  
-  console.log(`Application is running on port ${port}`);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
